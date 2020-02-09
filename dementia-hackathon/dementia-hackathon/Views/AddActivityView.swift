c4e45b56e44d29c2104573cd5da7cbbb28b811e4
Dmitry Leyko <dimaleyko@gmail.com>
2020-02-09 12:34:45 +0000

    more shit

--- dementia-hackathon/dementia-hackathon/Views/AddActivityView.swift
+++ dementia-hackathon/dementia-hackathon/Views/AddActivityView.swift
@@ -10,14 +10,60 @@ import SwiftUI
 
 struct AddActivityView: View {
     var body: some View {
-        VStack{
+        VStack(alignment: .leading){
+            Text("Add Your Activity:").bold().font(.system(size:34))
+            Spacer()
             HStack{
                 Text("Name: ")
+                Spacer()
                 Text("John")
-                .padding(3)
-                //.frame()
+                    .padding(.horizontal, 3.0)
+                    .frame(minWidth: 10)
+                    .border(Color.black)
             }
+            .padding(.vertical)
+            HStack{
+                Text("Surname: ")
+                Spacer()
+                Text("Smith")
+                .padding(.horizontal, 3.0)
+                .frame(minWidth: 10)
+                .border(Color.black)
+
+            }
+            .padding(.vertical)
+            HStack{
+                Text("Email: ")
+                Spacer()
+                Text("johnsmith@johnsmith.com")
+                .padding(.horizontal, 3.0)
+                .frame(minWidth: 10)
+                .border(Color.black)
+
+            }
+            .padding(.vertical)
+            HStack{
+                Text("Phone: ")
+                Spacer()
+                Text("07764726377")
+                .padding(.horizontal, 3.0)
+                .frame(minWidth: 10)
+                .border(Color.black)
+
+            }
+            .padding(.vertical)
+            HStack{
+                Text("Activity Name: ")
+                Spacer()
+                Text("Nordic Walking")
+                .padding(.horizontal, 3.0)
+                .frame(minWidth: 10)
+                .border(Color.black)
+
+            }
+            .padding(.vertical)
         }
+        .padding()
     }
 }
 
